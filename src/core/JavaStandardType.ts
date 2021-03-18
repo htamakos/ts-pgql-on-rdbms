415efd3bebd2443127f4f269cafe7c9d4e50bbec
htamakos <hironori.tamakoshi@oracle.com>
2021-03-18 12:36:12 +0900

    add LocalDateTime APIs

--- src/core/JavaStandardType.ts
+++ src/core/JavaStandardType.ts
@@ -30,6 +30,9 @@ export const JavaTimestamp = javaNodeApi.import('java.sql.Timestamp')
  * @category core-api
  */
 export class LocalDateTime {
+  public static MAX: LocalDateTime = new LocalDateTime(JavaLocalDateTime.MAX)
+  public static MIN: LocalDateTime = new LocalDateTime(JavaLocalDateTime.MIN)
+
   private static _cls: any = JavaLocalDateTime
   private static TIMESTAMP_FORMAT: string = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
   private static TIMESTAMP_FORMATTER = JavaDateTimeFormatter.ofPatternSync(
@@ -57,16 +60,144 @@ export class LocalDateTime {
     )
   }
 
-  constructor(javaLocalDateTIme: any) {
-    this.internalObj = javaLocalDateTIme
+  constructor(javaLocalDateTime: any) {
+    this.internalObj = javaLocalDateTime
   }
 
   toRawObject(): any {
     return this.internalObj
   }
 
+  getDayOfMonth(): number {
+    return this.internalObj.getDayOfMonthSync()
+  }
+
   getDayOfYear(): number {
     return this.internalObj.getDayOfYearSync()
+  }
+
+  getHour(): number {
+    return this.internalObj.getHourSync()
+  }
+
+  getMinute(): number {
+    return this.internalObj.getMinuteSync()
+  }
+
+  getMonthValue(): number {
+    return this.internalObj.getMonthValueSync()
+  }
+
+  getNano(): number {
+    return this.internalObj.getNanoSync()
+  }
+
+  getSecond(): number {
+    return this.internalObj.getSecondSync()
+  }
+
+  getYear(): number {
+    return this.internalObj.getYearSync()
+  }
+
+  hashCode(): number {
+    return this.internalObj.hashCodeSync()
+  }
+
+  minusDays(days: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.minusDaysSync(days))
+  }
+
+  minusHours(hours: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.minusHoursSync(hours))
+  }
+
+  minusMinutes(minutes: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.minusMinutesSync(minutes))
+  }
+
+  minusMonths(months: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.minusMonthsSync(months))
+  }
+
+  minusNanos(nanos: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.minusNanosSync(nanos))
+  }
+
+  minusSeconds(seconds: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.minusSecondsSync(seconds))
+  }
+
+  minusWeeks(weeks: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.minusWeeksSync(weeks))
+  }
+
+  minusYears(years: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.minusYearsSync(years))
+  }
+
+  plusDays(days: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.plusDaysSync(days))
+  }
+
+  plusHours(hours: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.plusHoursSync(hours))
+  }
+
+  plusMinutes(minutes: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.plusMinutesSync(minutes))
+  }
+
+  plusMonths(months: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.plusMonthsSync(months))
+  }
+
+  plusNanos(nanos: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.plusNanosSync(nanos))
+  }
+
+  plusSeconds(seconds: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.plusSecondsSync(seconds))
+  }
+
+  plusWeeks(weeks: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.plusWeeksSync(weeks))
+  }
+
+  plusYears(years: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.plusYearsSync(years))
+  }
+
+  withDayOfMonth(dayOfMonth: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.withDayOfMonthSync(dayOfMonth))
+  }
+
+  withDayOfYear(dayOfYear: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.withDayOfYearSync(dayOfYear))
+  }
+
+  withHour(hour: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.withHourSync(hour))
+  }
+
+  withMinute(minute: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.withMinuteSync(minute))
+  }
+
+  withMonth(month: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.withMonthSync(month))
+  }
+
+  withNano(nano: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.withNanoSync(nano))
+  }
+
+  withSecond(second: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.withSecondSync(second))
+  }
+
+  withYear(year: number): LocalDateTime {
+    return new LocalDateTime(this.internalObj.withYearSync(year))
   }
 
   toString(): string {
--- test/core/LocalDateTime.test.ts
+++ test/core/LocalDateTime.test.ts
@@ -23,4 +23,94 @@ describe('LocalDateTime', (): void => {
 
     expect(ldt != null).toBeTruthy()
   })
+
+  test('MAX_MIN', async () => {
+    expect(LocalDateTime.MAX.toString()).toStrictEqual(
+      '+999999999-12-31T23:59:59.999999999',
+    )
+    expect(LocalDateTime.MIN.toString()).toStrictEqual('-999999999-01-01T00:00')
+  })
+
+  test('get APIs', async () => {
+    const timestampStringValue: string = '2018-12-15 10:10:00+00:00'
+
+    const ldt: LocalDateTime = LocalDateTime.parseWithFormat(
+      timestampStringValue,
+      'yyyy-MM-dd HH:mm:ss+00:00',
+    )
+
+    expect(ldt.getYear()).toBe(2018)
+    expect(ldt.getMonthValue()).toBe(12)
+    expect(ldt.getDayOfMonth()).toBe(15)
+    expect(ldt.getHour()).toBe(10)
+    expect(ldt.getMinute()).toBe(10)
+    expect(ldt.getSecond()).toBe(0)
+    expect(ldt.getNano()).toBe(0)
+    expect(ldt.getDayOfYear()).toBe(349)
+  })
+
+  test('plus APIs', async () => {
+    const timestampStringValue: string = '2018-01-15 01:10:00+00:00'
+
+    const ldt: LocalDateTime = LocalDateTime.parseWithFormat(
+      timestampStringValue,
+      'yyyy-MM-dd HH:mm:ss+00:00',
+    )
+
+    expect(ldt.plusYears(1).getYear()).toBe(ldt.getYear() + 1)
+    expect(ldt.plusMonths(2).getMonthValue()).toBe(ldt.getMonthValue() + 2)
+    expect(ldt.plusDays(1).getDayOfMonth()).toBe(ldt.getDayOfMonth() + 1)
+    expect(ldt.plusHours(2).getHour()).toBe(ldt.getHour() + 2)
+    expect(ldt.plusMinutes(3).getMinute()).toBe(ldt.getMinute() + 3)
+    expect(ldt.plusSeconds(5).getSecond()).toBe(ldt.getSecond() + 5)
+    expect(ldt.plusNanos(3).getNano()).toBe(ldt.getNano() + 3)
+    expect(ldt.plusWeeks(2).getDayOfYear()).toBeGreaterThan(ldt.getDayOfYear())
+  })
+
+  test('minus APIs', async () => {
+    const timestampStringValue: string = '2018-08-15 08:10:10+00:00'
+
+    const ldt: LocalDateTime = LocalDateTime.parseWithFormat(
+      timestampStringValue,
+      'yyyy-MM-dd HH:mm:ss+00:00',
+    )
+
+    expect(ldt.minusYears(1).getYear()).toBe(ldt.getYear() - 1)
+    expect(ldt.minusMonths(2).getMonthValue()).toBe(ldt.getMonthValue() - 2)
+    expect(ldt.minusDays(1).getDayOfMonth()).toBe(ldt.getDayOfMonth() - 1)
+    expect(ldt.minusHours(2).getHour()).toBe(ldt.getHour() - 2)
+    expect(ldt.minusMinutes(3).getMinute()).toBe(ldt.getMinute() - 3)
+    expect(ldt.minusSeconds(5).getSecond()).toBe(ldt.getSecond() - 5)
+    expect(ldt.minusNanos(3).getNano()).toBe(999999997)
+    expect(ldt.minusWeeks(2).getDayOfYear()).toBeLessThan(ldt.getDayOfYear())
+  })
+
+  test('with APIs', async () => {
+    const timestampStringValue: string = '2018-08-15 08:10:10+00:00'
+
+    const ldt: LocalDateTime = LocalDateTime.parseWithFormat(
+      timestampStringValue,
+      'yyyy-MM-dd HH:mm:ss+00:00',
+    )
+
+    const withYear: number = 2021
+    const withMonth: number = 12
+    const withDayOfMonth: number = 8
+    const withDayOfYear: number = 180
+    const withHour: number = 9
+    const withMinute: number = 19
+    const withSecond: number = 34
+    const withNano: number = 8998
+
+    expect(ldt.withYear(withYear).getYear()).toBe(withYear)
+    expect(ldt.withMonth(withMonth).getMonthValue()).toBe(withMonth)
+    expect(ldt.withDayOfMonth(withDayOfMonth).getDayOfMonth()).toBe(
+      withDayOfMonth,
+    )
+    expect(ldt.withDayOfYear(withDayOfYear).getDayOfYear()).toBe(withDayOfYear)
+    expect(ldt.withHour(withHour).getHour()).toBe(withHour)
+    expect(ldt.withMinute(withMinute).getMinute()).toBe(withMinute)
+    expect(ldt.withSecond(withSecond).getSecond()).toBe(withSecond)
+    expect(ldt.withNano(withNano).getNano()).toBe(withNano)
+  })
 })
